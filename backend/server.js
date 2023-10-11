716e5ad42e0a9f7a52f82506a988a215709d9a10
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-10-11 22:04:47 +0300

    Added Expess router.

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
+process.env;
+
 const express = require("express");
+const workoutRoutes = require("./routes/workouts.js");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
+    console.log(req.method, req.path);
     next();
 })
 
 // routes
-app.get("/", (req, res) => {
-    res.json({mssg: "Welcome to the page!"});
-})
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
     console.log(`listening on localhost:${process.env.PORT}`);
 });
-
-process.env;
